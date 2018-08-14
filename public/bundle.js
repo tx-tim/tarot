4752a54efd0e3f798e77f20469144e056b6b7bbb
Tim Proffitt <tim.proffitt@gmail.com>
2018-08-13 20:00:50 -0500

    style the cards a little

--- public/bundle.js
+++ public/bundle.js
@@ -10,11 +10,28 @@ import { TarotDeck } from './deck.js';
 
   window.onload = function()
     {
+
+        const spreadStyles = {
+            padding: '50px',
+            display: 'flex'
+
+        };
+        const cardStyles = {
+            width: '180px',
+            background: 'black',
+            color: 'white',
+            margin: '10px',
+            padding: '5px'
+        };
+
         class Card extends React.Component
         {
             render()
             {
-                return React.createElement('div', null, `card: ${this.props.name}`);
+                return React.createElement('div', {style: cardStyles, key: this.props.name}, [
+                    React.createElement('p', {key: `${this.props.name}-title`}, `card: ${this.props.name}`),
+                    React.createElement('img', {key: `${this.props.name}-img`, style: {width: '100%'},src: this.props.image})
+                ]);
             }
         }
 
@@ -26,11 +43,14 @@ import { TarotDeck } from './deck.js';
                 var shuffledDeck = deck.shuffle();
                 var hand = [];
                 hand = deck.deal(shuffledDeck, 3);
-                return hand.map((item) => {
-                    return React.createElement(Card, {name : item.name })
-                });
+                return React.createElement('div', {style: spreadStyles}, 
+                    hand.map((item) => {
+                        return React.createElement(Card, {key: item.name, name : item.name, image: item.image})
+                    })
+                )
             }
         }
+
         ReactDOM.render(
             React.createElement('div', null, 
                 React.createElement(Spread),
